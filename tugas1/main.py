f591797bffd3c948863e336d2380db9123ca4767
Mufid25 <mufidmuhammad189@gmail.com>
2024-10-19 19:06:39 +0700

    menambahkan keterangan yang kurang

--- tugas1/main.py
+++ tugas1/main.py
@@ -9,27 +9,27 @@ def parse(text: str):
     inside_tag = False # utk mengetauhi ketika berada di dalam tag html
     tag_type = None  # menyimpan jenis tag yg ditemukan
 
-    for char_info in indexed_text:
-        char = char_info['char']
-        index = char_info['index']  
+    for char_info in indexed_text: # utk melakukan perulangan karakter dan informasi index dari hasil "indexed_text"
+        char = char_info['char'] # utk mendapatkan cahrakter
+        index = char_info['index']  # utk mendapatkan index
         
 
         if char == '<': 
-            inside_tag = True
+            inside_tag = True # utk menandakan bahwa kita sekarang sedang berada didalam tag
             tag_type = ""
         elif char == '>':  
-            inside_tag = False
+            inside_tag = False # utk menandakan bahwa kita telah keluar dari tag  
             if tag_type == "b":  
-                start = real_index
+                start = real_index # menyimpan index awal text bold
             elif tag_type == "/b":  
                 highlights.append({"start": start, "end": real_index})
-                start = None
-            tag_type = None
+                start = None # mereset variable 'start' setelah menemukan akhir dari text bold
+            tag_type = None # mereset variable 'tag_type' setelah selesai
         elif inside_tag:  
-            tag_type += char
+            tag_type += char # utk menambahkan karakter ke 'tag_type' untuk mengidentifikasi tipe tag
         else:
             
-            final_text.append(char)
+            final_text.append(char) # utk menambah karakter ke dalam 'final_test'
             real_index += 1 #utk melacak posisi karakteer dalam text bersih
 
     clean_text = ''.join(final_text) # utk menggabungkan list (final_text) menjadi string text bersih
@@ -38,7 +38,7 @@ def parse(text: str):
         "highlights": highlights
     }
 
-    return result
+    return result # utk mengembalikan hasil dalam bentuk directory yang berisi teks bersih dan highlight
 
 def main():
     test1 = "aku sedang <b>belajar</b> python"
